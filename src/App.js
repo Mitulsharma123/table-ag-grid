ff92b2df0b2eb09b8aee4979b9f532261cec5de2
Mitulsharma123 <sharmamitul72@gmail.com>
2022-11-02 18:21:28 +0530

    update readme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { render } from 'react-dom';
+import { AgGridReact } from 'ag-grid-react';
+import './App.css'; 
+
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
 
 function App() {
+  const [rowData, setRowData] = useState([]);
+  
+  useEffect(() => {
+    fetch('data.json')
+      .then(res => res.json())
+      .then(result => setRowData(result.data));
+  }, []);
+
+  /*const [columnDefs, setColumnDefs] = useState([
+       { field: 'casenumber', sortable: 'true', fiter: 'true'},
+       { field: 'product', sortable: 'true', fiter: 'true'},
+       { field: 'status', sortable: 'true', fiter: 'true'}
+       { field: 'certification type', sortable: 'true', fiter: 'true'}
+       { field: 'ecosystem', sortable: 'true', fiter: 'true'}
+       { field: 'ladt updated', sortable: 'true', fiter: 'true'}
+   ]);*/
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+     <div className="ag-theme-alpine" 
+     style={{height: 400, width: 6000}}>
+           <AgGridReact
+                defaultColDef={{ flex: 1 }}
+                rowHeight={60}
+                rowData={rowData} >
+                <AgGridColumn field="first_name" headerName="First Name" sortable={true} filter={true} cellClass="vertical-middle" />
+                <AgGridColumn field="last_name" headerName="Last Name" sortable={true} filter={true} cellClass="vertical-middle" />
+                <AgGridColumn field="email" headerName="Email" sortable={true} filter={true} cellClass="vertical-middle" />
+           </AgGridReact>
+       </div>
+   );
+};
 
 export default App;
